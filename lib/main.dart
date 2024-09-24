cd3fa42bf1ba42272be22be57c6ae2d0b9df863e
HE41T <natagon2546@gmail.com>
2024-09-24 12:18:03 +0700

    ใหม่

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'image_section.dart';
 import 'text_section.dart';
 import 'title_section.dart';
 
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -32,20 +31,19 @@ class MyApp extends StatelessWidget {
               ),
               ButtonSection(),
               TextSection(
-              description:
-                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
-                  'Bernese Alps. Situated 1,578 meters above sea level, it '
-                  'is one of the larger Alpine Lakes. A gondola ride from '
-                  'Kandersteg, followed by a half-hour walk through pastures '
-                  'and pine forest, leads you to the lake, which warms to 20 '
-                  'degrees Celsius in the summer. Activities enjoyed here '
-                  'include rowing, and riding the summer toboggan run.',
-            ),
+                description:
+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
             ],
+          ),
         ),
-      ),
       ),
     );
   }
 }
-
